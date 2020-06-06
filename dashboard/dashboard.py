da8ac7a93733ff5f24f5c2d22b51f2d573ba805d
SSANGMAN <qkrtkdals93@gmail.com>
2020-06-06 17:18:55 +0900

    Update

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,7 +1,12 @@
-import streamlit as st
-import pymysql
 import pandas as pd
 import numpy as np
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import pymysql
+import streamlit as st
+
 import datetime
 
 today = datetime.date.today()
@@ -63,7 +68,7 @@ if start_date > end_date:
 
 time_interval = st.sidebar.radio(
     'Time Interval (Hour): Choose Select how often you want to check the information.', options = time_range,
-    index = 2)
+    index = 0)
 ## Styling
 st.sidebar.header("Styling")
 st.sidebar.markdown(""" Choose Styling about Visualization """)
@@ -77,6 +82,7 @@ report_type = st.sidebar.radio(
 )
 
 # Main Page
+## DataFrame
 st.header("DataFrame")
 st.markdown(
     """
@@ -92,13 +98,16 @@ if component_box == 'CPU':
     select_brand = st.selectbox("Brand", ["All", "Intel", "AMD"])
 
     if select_brand == 'All':
+        brand_df = filter_df
         st.dataframe(filter_df)
 
     else:
         if select_brand == "Intel":
+            brand_df = filter_df[filter_df['NAME'].str.contains(r'인텔')]
             st.dataframe(filter_df[filter_df['NAME'].str.contains(r'인텔')])
         
         elif select_brand == 'AMD':
+            brand_df = filter_df[filter_df['NAME'].str.contains(r'AMD')]
             st.dataframe(filter_df[filter_df['NAME'].str.contains(r'AMD')])
 
 elif component_box == "GPU":
@@ -107,6 +116,64 @@ elif component_box == "GPU":
     select_brand = st.selectbox("Brand", brand_list)
 
     if select_brand == 'All':
-        st.dataframe(filter_df)
+        brand_df = filter_df
+        st.dataframe(brand_df)
     else:
-        st.dataframe(filter_df.loc[df['BRAND'] == select_brand])
+        brand_df = filter_df.loc[df['BRAND'] == select_brand]
+        st.dataframe(brand_df)
+## Line Plot
+st.header("Line Plot")
+st.markdown(
+    """
+    Show the Bar plot Which brand is more popular? for the selected time period.
+
+    If there is no data collected during the specified period, an error will occur.
+    """
+)
+### Line Plot Preprocessing
+def LinePlotPreprocess(data, process_type):
+    if process_type == 'price':
+        line_df = pd.pivot_table(data, index = ['CRAWL_DATE', 'HOUR'], columns = 'NAME', values = 'PRICE').reset_index()
+    
+    elif process_type == 'rank':
+        line_df = pd.pivot_table(data, index = ['CRAWL_DATE', 'HOUR'], columns = 'NAME', values = 'RANKING').reset_index()
+    
+    tmp_time_list = []
+    for i in range(len(line_df)):
+        time = pd.to_datetime(line_df['HOUR'], format = '%H').astype(str).str.split(" ")[i][1]
+        tmp_time_list.append(time)
+
+    line_df['DATE'] = line_df['CRAWL_DATE'].astype(str) + " " + tmp_time_list
+    line_df.drop(columns = ['CRAWL_DATE', 'HOUR'], inplace = True)
+    line_df.set_index("DATE", inplace = True)
+    line_df.columns.name = None
+
+    return line_df
+#### About Ranking
+st.subheader("RANKING")
+st.markdown(
+    """ 
+    Shows the ranking changes of products during the specified period.
+    """
+)
+#### About Price
+ranking_df = LinePlotPreprocess(brand_df, process_type = 'rank')
+### Visualization
+try:
+    st.line_chart(ranking_df)
+except ValueError:
+    st.error("Invalid period specification, or no collected data exists. Please re-specify the period.")
+
+st.subheader("Price")
+st.markdown(
+    """ 
+    Shows the price changes of products during the specified period.
+    """
+)
+#### About Price
+price_df = LinePlotPreprocess(brand_df, process_type = 'price')
+### Visualization
+try:
+    st.line_chart(price_df)
+except ValueError:
+    st.error("Invalid period specification, or no collected data exists. Please re-specify the period.")
